3186d1a6f5c0f34d4fbc380ae87867d7089df5f4
Vanessa Kroeker <vkroeker@gmail.com>
2019-10-14 22:49:00 +0000

    Added assertEqual.js which compares two values and prints a message indicating if they match or not

---
+++ assertEqual.js
@@ -0,0 +1,25 @@
+/*
+Receives two values,
+Prints a message indicating if they match or not.
+*/
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅ ✅ ✅ Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("😡 😡 😡 Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(1, "1");
+assertEqual("hello world", "hello world");
+assertEqual("hello world ", "hello world");
+assertEqual("hello world", "Hello world");
+assertEqual(10, 10.00);
+assertEqual(0, "");
+assertEqual(0, " ");
+assertEqual(NaN, "hi");
+assertEqual((2 * 2), 4);
