abda582dea0af85c0508479d7c6003b760e90dd2
Vanessa Kroeker <vkroeker@gmail.com>
2019-10-16 05:54:03 +0000

    flatten function completed

--- flatten.js
+++ flatten.js
@@ -29,3 +29,25 @@ const assertArraysEqual = function(array1, array2) {
   }
 };
 
+/* 
+Receives an array of arrays
+Returns a flattened version of the array
+*/
+
+const flatten = function(arr) {
+  let flattenedArray = [];
+  for (let i = 0; i < arr.length; i++) {
+    let thisValue = arr[i];
+    if (!Array.isArray(thisValue)) {
+      flattenedArray.push(thisValue);
+    } else for (let n = 0; n < thisValue.length; n++) {
+      let nestedValue = thisValue[n];
+      flattenedArray.push(nestedValue);
+    }
+  }
+  return flattenedArray;
+};
+
+
+assertArraysEqual(flatten([]), []);
+assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]);
