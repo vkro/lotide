065c60cdcf6be4997af71e51cf00f9903e9c604c
Vanessa Kroeker <vkroeker@gmail.com>
2019-10-16 21:13:07 +0000

    added countLetters.js no code yet

---
+++ countLetters.js
@@ -0,0 +1,16 @@
+/*
+Receives two values,
+Prints a message indicating if they match or not.
+*/
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ ✅ ✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`😡 😡 😡 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+/*
+
+*/
