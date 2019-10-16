1e625fa20bd40cdfa3fe428207e8694bc8584c8b
Vanessa Kroeker <vkroeker@gmail.com>
2019-10-16 20:26:09 +0000

    countOnly counts strings in array that match  truthy obj keys

---
+++ countOnly.js
@@ -0,0 +1,50 @@
+/*
+Receives two values,
+Prints a message indicating if they match or not.
+*/
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ ✅ ✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`😡 😡 😡 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+/*
+Receives an array strings to count, and an object specifying what to count.
+Returns an object containing counts of values in the string array that
+match keys in the object - but only those keys that have a truthy value.
+*/
+
+const countOnly = function(allItems, itemsToCount){
+  let results = {};
+  let howMany = 0
+
+  for (const item of allItems) {
+    if (results[item]) {
+      results[item] +=1;
+    } else {
+      results[item] = 1;
+    }
+  }
+  return results;
+};
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
