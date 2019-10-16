46df321f35999cfeafdf3b4c7398ebf77417e91a
Vanessa Kroeker <vkroeker@gmail.com>
2019-10-16 23:42:37 +0000

    findKeyByValue function description and tests

---
+++ findKeyByValue.js
@@ -0,0 +1,37 @@
+/*
+Receives two values,
+Prints a message indicating if they match or not.
+*/
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ ✅ ✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`😡 😡 😡 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+/*
+Receives an object and value
+Return first key in object that contains value
+Otherwise, return undefined
+*/
+
+const findKeyByValue = function(obj, key) {
+
+};
+
+
+//TESTS
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sci-fi");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine Nine"), undefined);
