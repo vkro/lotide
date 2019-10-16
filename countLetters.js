a006bc0e650d4755dcade8fe4e34ae371ad26012
Vanessa Kroeker <vkroeker@gmail.com>
2019-10-16 21:38:22 +0000

    countLetters returns object indicating count of each letter in a string

--- countLetters.js
+++ countLetters.js
@@ -23,7 +23,14 @@ const countLetters = function(allLetters) {
   results = {};
   //iterate over the string
   for (let char of allLetters) {
-    console.log(char);
+    if (char != " ") {
+      if (results[char]) {
+        results[char] += 1;
+      }
+      else {
+        results[char] = 1;
+      }
+    }
   }
   return results;
 };
@@ -31,10 +38,15 @@ const countLetters = function(allLetters) {
 
 const testLetters = "js for of loop";
 
+console.log("**** The result of calling countLetters on \"js for loop\" *****");
 const result1 = countLetters(testLetters);
+console.log(result1);
+console.log("****** End of result, now for tests ******")
+
 
 assertEqual(result1["j"], 1);
 assertEqual(result1["s"], 1);
 assertEqual(result1["f"], 2);
 assertEqual(result1["o"], 4);
+assertEqual(result1["l"], 1);
 assertEqual(result1["x"], undefined);
