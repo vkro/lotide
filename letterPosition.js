2dcc35e49c4a293a153e036c6810fabb8eb6d3c1
Vanessa Kroeker <vkroeker@gmail.com>
2019-10-16 22:18:40 +0000

    letterPosition new tests to compare arrays

--- letterPosition.js
+++ letterPosition.js
@@ -1,13 +1,31 @@
 /*
-Receives two values,
-Prints a message indicating if they match or not.
+Receives two arrays,
+Returns true or false, based on a perfect match.
 */
 
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ ✅ ✅ Assertion Passed: ${actual} === ${expected}`);
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length === arr2.length) {
+    let equalSoFar = true;
+    for (let i = 0; i < arr1.length; i++) {
+      if (equalSoFar === true) {
+        if (arr1[i] !== arr2[i]) {
+          equalSoFar = false;
+        }
+      }
+    }
+    return equalSoFar;
+  } else return false;
+};
+
+/*
+Receives two arrays
+Logs a message in the console about whether they are equal or not
+*/
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`✅ ✅ ✅ Assertion Passed: ${array1} === ${array2}`);
   } else {
-    console.log(`😡 😡 😡 Assertion Failed: ${actual} !== ${expected}`);
+    console.log(`😡 😡 😡 Assertion Failed: ${array1} !== ${array2}`);
   }
 };
 
@@ -15,7 +33,7 @@ const assertEqual = function (actual, expected) {
 Receives a string
 Return an object listing each unique character,
 with an array of all of its indices (zero-based positions)
-in the string
+in the string, not counting spaces
 */
 
 const letterPosition = function(sentence) {
@@ -24,14 +42,14 @@ const letterPosition = function(sentence) {
   return results;
 };
 
-const testLetters = "how about this string how does this work";
+const testLetters = "hello";
+
+// TEST CODE
+assertArraysEqual(result1['h'], [0]);
+assertArraysEqual(result1['e'], [1]);
+assertArraysEqual(result1['l'], [2, 3]);
+assertArraysEqual(result1['o'], [4]);
 
 console.log("**** The result of calling countLetters on testLetters *****");
-const result1 = countLetters(testLetters);
+const result1 = letterPosition(testLetters);
 console.log(result1);
-console.log("****** End of result, now for tests ******");
-
-
-assertEqual(result1["h"], [0, 11, 22]);
-assertEqual(result1["s"], [13, 15, 29]);
-assertEqual(result1["x"], undefined);
