52ba0ffd880f054f77127c003d2b8e8c2a855b9c
Vanessa Kroeker <vkroeker@gmail.com>
2019-10-17 00:49:36 +0000

    extract keys to array and check for equal length

--- eqObjects.js
+++ eqObjects.js
@@ -15,10 +15,20 @@ const assertEqual = function(actual, expected) {
 Accepts two objects
 Returns true if both objects have identical keys with identical values.
 Otherwise returns false
+
+Two objects are equal when:
+- they have the same number of keys
+- the value for each key is the same in both objects
 */
 
 const eqObjects = function(object1, object2) {
+  let keys1 = Object.keys(object1);
+  let keys2 = Object.keys(object2);
+  // do they have the same number of keys?
+  if (keys1.length === keys2.length) {
 
+
+  } else return false;
 };
 
 //TESTS
