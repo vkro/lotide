1662ad898a619a1968e74b0a916f69d84773b575
Vanessa Kroeker <vkroeker@gmail.com>
2019-10-17 00:23:52 +0000

    added tests to eqObjects

--- eqObjects.js
+++ eqObjects.js
@@ -16,3 +16,23 @@ Accepts two objects
 Returns true if both objects have identical keys with identical values.
 Otherwise returns false
 */
+
+const eqObjects = function(object1, object2) {
+
+};
+
+//TESTS
+
+const ab = {a: "1", b: "2"};
+const ba = {b: "2", a: "1"};
+const abc = {a: "1", b: "2", c: "3"};
+const cba = {a: 1, b: 2, c: "3"}
+const emptyObj = {};
+const emptyObj2 = {};
+
+assertEqual(eqObjects(ab, ba), true);
+assertEqual(eqObjects(ab, abc), false);
+assertEqual(eqObjects(abc, cba), false);
+assertEqual(eqObjects(emptyObj, abc), false);
+assertEqual(eqObjects(emptyObj, emptyObj2), true);
+
