3a0968e937cd3846b429c48a8c2f60b745b45f45
Vanessa Kroeker <vkroeker@gmail.com>
2019-10-18 00:29:40 +0000

    function definition and declaration for takeUntil

---
+++ takeUntil.js
@@ -0,0 +1,12 @@
+// Takes 2 parameters:
+// - array
+// - callback (Lodash calls this the "predicate")
+// Returns a slice of the array, with elements taken
+// from the beginning, until the callback/predicate
+// returns a truthy value.
+// Callback is giben only one value - the current item in array
+
+
+const takeUntil = function(array, callback) {
+
+};
