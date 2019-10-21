1f66379143e7cdbb78e317ce90f5db23cfbf0567
Vanessa Kroeker <vkroeker@gmail.com>
2019-10-21 20:54:52 +0000

    added mocha & chai to lotide proj, converted headTest.js to use m & c

--- test/headTest.js
+++ test/headTest.js
@@ -1,10 +1,27 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const head = require('../head');
 
 
-// TEST CODE
-assertEqual(head([0, 1, 2, 3]), 0);
-assertEqual(head([1]), 1);
-assertEqual(head(["hey", "this", "is", "an", "array"]), "hey");
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head([]), undefined);
+describe("#head", () => {
+  
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+
+  it("returns '0' for [0, 1, 2, 3]", () => {
+    assert.strictEqual(head([0, 1, 2, 3]), 0); 
+  });
+
+  it("returns 'hey' for ['hey', 'this', 'is', 'an', 'array']", () => {
+    assert.strictEqual(head(['hey', 'this', 'is', 'an', 'array']), "hey"); 
+  });
+
+  it("returns 'undefined' for []", () => {
+    assert.strictEqual(head([]), undefined); 
+  });
+
+});
