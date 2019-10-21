78b5a0de25a0cc4c747381b0d7db80004a4c863f
Vanessa Kroeker <vkroeker@gmail.com>
2019-10-21 20:11:08 +0000

    moved tests from assertArraysEqual to new test tile

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,21 +1,5 @@
-/*
-Receives two arrays,
-Returns true or false, based on a perfect match.
-*/
+const eqArrays = require('./eqArrays');
 
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length === arr2.length) {
-    let equalSoFar = true;
-    for (let i = 0; i < arr1.length; i++) {
-      if (equalSoFar === true) {
-        if (arr1[i] !== arr2[i]) {
-          equalSoFar = false;
-        }
-      }
-    }
-    return equalSoFar;
-  } else return false;
-};
 
 /*
 Receives two arrays
@@ -29,15 +13,5 @@ const assertArraysEqual = function(array1, array2) {
   }
 };
 
-// TEST CODE
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual([1, 2, 3], [1, 2]);
-assertArraysEqual([], []);
-assertArraysEqual(["a", "b", "c", "d"],["a", "b", "c", "d"]);
-assertArraysEqual(["a", "b", "c", "d"],["a", "b", "c", "c"]);
-assertArraysEqual(["a", "d", "c", "d"],["a", "b", "c", "d"]);
-assertArraysEqual(["a", "d", "c", "d"], [1, 2, 3, 4]);
-assertArraysEqual([1, 2, 4, 5], [1, 2, 3, 4]);
-assertArraysEqual([9, 2, 3, 4], [1, 2, 3, 4]);
 
-
+module.exports = assertArraysEqual;
