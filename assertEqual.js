d2ad0060a8fbff01e63d95edb380c2ec318b5fcd
Vanessa Kroeker <vkroeker@gmail.com>
2019-10-21 19:42:20 +0000

    removed tests and added module exports

--- assertEqual.js
+++ assertEqual.js
@@ -11,15 +11,5 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(1, "1");
-assertEqual("hello world", "hello world");
-assertEqual("hello world ", "hello world");
-assertEqual("hello world", "Hello world");
-assertEqual(10, 10.00);
-assertEqual(0, "");
-assertEqual(0, " ");
-assertEqual(NaN, "hi");
-assertEqual((2 * 2), 4);
+
+module.exports = assertEqual;
--- head.js
+++ head.js
@@ -1,24 +1,9 @@
-/*
-Receives an array,
-Returns the first item in the array.
-*/
+const assertEqual = require('./assertEqual');
 
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ ✅ ✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`😡 😡 😡 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
+// Returns the first item in an array
 
 const head = function(arr) {
   return arr[0];
 };
 
-// TEST CODE
-assertEqual(head([0, 1, 2, 3]), 0);
-assertEqual(head([1]), 1);
-assertEqual(head(["hey", "this", "is", "an", "array"]), "hey");
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head([]), undefined);
+module.exports = head;
