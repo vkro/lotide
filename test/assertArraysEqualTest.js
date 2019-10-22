e24b3cf73ba151b7a566fb94b60300a6e4d0b185
Vanessa Kroeker <vkroeker@gmail.com>
2019-10-22 01:05:56 +0000

    added assertArraysEqualTest.js to test dir

---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,15 @@
+
+const eqArrays = require('../eqArrays');
+const assertArraysEqual = require('../assertArraysEqual');
+const assertEqual = require('../assertEqual');
+
+// TEST CODE
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2, 3], [1, 2]);
+assertArraysEqual([], []);
+assertArraysEqual(["a", "b", "c", "d"],["a", "b", "c", "d"]);
+assertArraysEqual(["a", "b", "c", "d"],["a", "b", "c", "c"]);
+assertArraysEqual(["a", "d", "c", "d"],["a", "b", "c", "d"]);
+assertArraysEqual(["a", "d", "c", "d"], [1, 2, 3, 4]);
+assertArraysEqual([1, 2, 4, 5], [1, 2, 3, 4]);
+assertArraysEqual([9, 2, 3, 4], [1, 2, 3, 4]);
