40406657e2d3d35eac572c62fc5a6da8bb5446d1
Vanessa Kroeker <vkroeker@gmail.com>
2019-10-22 04:17:38 +0000

    deleted old tests from map function file

--- test/mapTest.js
+++ test/mapTest.js
@@ -17,20 +17,3 @@ describe("#map", () => {
   });
 
 });
-
-
-
-
-// TESTS  
-// const words = ['sneezed', 'on', 'the', 'beat', 'and', 'the', 'beat', 'got', 'sicker']; 
-// const results1 = map(words, word => word[0]);
-// console.log(results1);
-// 
-// 
-// assertArraysEqual(map(words, word => word.length), [7, 2, 3, 4, 3, 3, 4, 3, 6]);
-// assertArraysEqual(map(words, word => word[0]), ['s', 'o', 't', 'b', 'a', 't', 'b', 'g', 's']);
-// assertArraysEqual(map([1, 2, 3, 0], x => x * 3), [3, 6, 9, 0]);
-// assertArraysEqual(map(['heeler', 'corgi', 'labradoodle'], word => `${word}s`), ['heelers', 'corgis', 'labradoodles']);
-// assertArraysEqual(map([1, 2, 3, 0], x => x.toString()), ['1', '2', '3', '0']);
-// 
-// 
