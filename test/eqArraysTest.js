1ac24aff1d6a7519c23df9017a7cc1daa2a65478
Vanessa Kroeker <vkroeker@gmail.com>
2019-10-22 02:51:35 +0000

    refactored eqArrays tests to use Mocha & Chai

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,14 +1,33 @@
 const eqArrays = require('../eqArrays');
-const assertArraysEqual = require('../assertArraysEqual');
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
+const expect = require('chai').expect;
+const be = require('chai').be;
 
-// TEST CODE
-assertEqual(eqArrays([], []), true);
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [1, 2]), false);
-assertEqual(eqArrays(["a", "b", "c", "d"],["a", "b", "c", "d"]), true);
-assertEqual(eqArrays(["a", "b", "c", "d"],["a", "b", "c", "c"]), false);
-assertEqual(eqArrays(["a", "d", "c", "d"],["a", "b", "c", "d"]), false);
-assertEqual(eqArrays(["a", "d", "c", "d"], [1, 2, 3, 4]), false);
-assertEqual(eqArrays([1, 2, 4, 5], [1, 2, 3, 4]), false);
-assertEqual(eqArrays([9, 2, 3, 4], [1, 2, 3, 4]), false);
+
+
+describe("#eqArrays", () => {
+  
+  it("returns true for two empty arrays", () => {
+    assert(eqArrays([], []));
+  });
+
+  it("returns true for two arrays with identical values at identical indices", () => {
+    assert(eqArrays([1, 2, 3, 4, 5, 6], [1, 2, 3, 4, 5, 6]));
+  });
+  
+  it("returns false for two arrays with identical values at different indices", () => {
+    expect(eqArrays([1, 2, 3, 4, 5, 6], [1, 2, 3, 4, 6, 5])).to.be.false;
+  });
+
+  it("returns false for two arrays of different lengths", () => {
+    expect(eqArrays([1, 2, 3, 4, 5, 6], [1, 2, 3, 4])).to.be.false;
+  });
+
+  it("returns false for two arrays with different values", () => {
+    expect(eqArrays([1, 2, 3, 4], ['a', 'b', 'c', 'd'])).to.be.false;
+  });
+
+});
+
+
+
