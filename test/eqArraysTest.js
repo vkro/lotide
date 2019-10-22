3702e0f38a9fedf4d1a03bdcd8ffd2bda972c78b
Vanessa Kroeker <vkroeker@gmail.com>
2019-10-22 01:04:04 +0000

    added empty arr and matching num array tests back to eqArraysTest

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,11 +1,11 @@
-const assertEqual = require('../assertEqual');
 const eqArrays = require('../eqArrays');
+const assertArraysEqual = require('../assertArraysEqual');
+const assertEqual = require('../assertEqual');
 
 // TEST CODE
-
+assertEqual(eqArrays([], []), true);
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
 assertEqual(eqArrays([1, 2, 3], [1, 2]), false);
-assertEqual(eqArrays([], []), true);
 assertEqual(eqArrays(["a", "b", "c", "d"],["a", "b", "c", "d"]), true);
 assertEqual(eqArrays(["a", "b", "c", "d"],["a", "b", "c", "c"]), false);
 assertEqual(eqArrays(["a", "d", "c", "d"],["a", "b", "c", "d"]), false);
