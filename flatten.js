fdddd3f687fab42fde3ecceec64e8fea32d075b6
Vanessa Kroeker <vkroeker@gmail.com>
2019-10-23 01:38:15 +0000

    changed c-style for loops to for...in loops, removed redundant code

--- flatten.js
+++ flatten.js
@@ -1,22 +1,31 @@
-const assertArraysEqual = require('./assertArraysEqual');
-
-/* 
+/*
 Accepts an array of arrays
 Returns a flattened version of the array
 */
 
 const flatten = function(arr) {
   let flattenedArray = [];
-  for (let i = 0; i < arr.length; i++) {
-    let thisValue = arr[i];
-    if (!Array.isArray(thisValue)) {
-      flattenedArray.push(thisValue);
-    } else for (let n = 0; n < thisValue.length; n++) {
-      let nestedValue = thisValue[n];
-      flattenedArray.push(nestedValue);
+  for (let item in arr) {
+    if (!Array.isArray(item)) {
+      flattenedArray.push(item);
+    } else for (let nestedItem in item) {
+      flattenedArray.push(nestedItem);
     }
   }
   return flattenedArray;
 };
+// const flatten = function(arr) {
+//   let flattenedArray = [];
+//   for (let i = 0; i < arr.length; i++) {
+//     let thisValue = arr[i];
+//     if (!Array.isArray(thisValue)) {
+//       flattenedArray.push(thisValue);
+//     } else for (let n = 0; n < thisValue.length; n++) {
+//       let nestedValue = thisValue[n];
+//       flattenedArray.push(nestedValue);
+//     }
+//   }
+//   return flattenedArray;
+// };
 
 module.exports = flatten;
